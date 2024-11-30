4dc4217423df49d960c5b7190ef5b6b3662c29a8
Mr.js01 <78050688+MrJs01@users.noreply.github.com>
2024-11-30 02:20:18 +0000

    a

--- yii2/components/GeminiAgent.php
+++ yii2/components/GeminiAgent.php
@@ -8,7 +8,7 @@ use yii\base\Exception;
 use yii\httpclient\Client;
 class GeminiAgent extends Component
 {
-    public $apiKey = "AIzaSyBA0a5bUWHG8LyHJmvgXzJsa4ZpS4L-ayc";
+    public $apiKey = "AIzaSyAuW8n3aaCPKO1bdIT1NR4f-mSy7ttxrYQ";
     public $apiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/';
     public $retryDelay = 15;
     public $maxRetries = 3;
